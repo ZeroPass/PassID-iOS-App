3a274af6c0ab9e523207c9afe4ce4e7b3a758e54
smlu <smlu@s5.net>
2019-12-12 18:22:04 +0100

    Add SettingsButton.swift

---
+++ PassID/view/SettingsButton.swift
@@ -0,0 +1,92 @@
+//
+//  StettingsButton.swift
+//  PassID
+//
+//  Created by smlu on 03/12/2019.
+//  Copyright © 2019 ZeroPass. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct SettingsSheet: View {
+    @EnvironmentObject var settings: SettingsStore
+
+    @State private var showActivity = false
+    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
+    var body: some View {
+        ActivityView(msg: "Trying to connect to server ...", showActivity: $showActivity) {
+            // Note: when activity is visible it won't cover the whole screen when using this view as modal popup.
+            NavigationView {
+                Form {
+                    Section(header: Text("PassID Server")) {
+                        TextEdit(label: "Server URL", value: self.$settings.url)
+                            .defaultValue(SettingsStore.DEFAULT_URL)
+                            .inputValidator({ v in return Utils.isValidUrl(v, forceAddressAndPort: false) })
+                            .keyboardType(.URL)
+                            .textContentType(.URL)
+                        TextEdit(label: "Timeout", placeholder: String(SettingsStore.DEFAULT_TIMEOUT), value: self.$settings.timeout)
+                            .defaultValue(SettingsStore.DEFAULT_TIMEOUT)
+                            .keyboardType(.decimalPad)
+                            .minValue(1.0)
+                            .maxValue(99.9)
+                    }
+
+                    Button(action: {
+                        self.showActivity = true
+                        let rpc = JRPClient(url: self.settings.url, defaultTimeout: self.settings.timeout)
+                        rpc.call(method: "passID.ping", params: ["ping" : 5]) { result in
+                            self.showActivity = false
+                            if ((try? result.get()) != nil) {
+                                Toast.show(message: "Connection succeeded!")
+                            }
+                            else {
+                                Toast.show(message: "Connection failed!")
+                            }
+                        }
+                    }) {
+                        HStack(alignment: .center) {
+                            Spacer()
+                            Text("Test Connection")
+                            Spacer()
+                        }
+                    }
+                    .padding()
+                }
+                .navigationBarTitle(Text("Settings"))
+                .navigationBarItems(trailing: Button(action: { self.presentationMode.wrappedValue.dismiss() }) {
+                    Text("Close")
+                })
+            }
+        }
+    }
+}
+
+
+struct SettingsButton : View {
+    var settings: SettingsStore = SettingsStore()
+    
+    @State private var showSheet = false
+    var body: some View {
+        Button(action: { self.showSheet = true }){
+            Text("Settings")
+        }
+    .sheet(isPresented: $showSheet,
+               onDismiss: { self.showSheet = false }) {
+                SettingsSheet().environmentObject(self.settings)
+        }
+    }
+}
+
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SettingsSheet()
+                .environmentObject(SettingsStore())
+                .environment(\.colorScheme, .dark)
+            SettingsButton()
+                .environment(\.colorScheme, .dark)
+        }
+    }
+}
