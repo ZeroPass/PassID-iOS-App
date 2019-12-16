641f5c5274bd1d91cc209146e0921bc5c73ee1a3
smlu <smlu@s5.net>
2019-12-16 23:28:32 +0100

    Add proto challenge structs

---
+++ PassID/proto/ProtoChallenge.swift
@@ -0,0 +1,80 @@
+//
+//  ProtoChallenge.swift
+//  PassID
+//
+//  Created by smlu on 16/12/2019.
+//  Copyright © 2019 ZeroPass. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+struct CID : ProtoObject {
+    static let serKey = "cid"
+    var data: Data
+    
+    init?(data: Data) {
+        if(data.count != 4) {
+            return nil
+        }
+        self.data = data
+    }
+
+    init?(json: JSON) {
+        guard let rawCID = json[ProtoChallenge.serKey].string?.toData() else {
+            return nil
+        }
+        self.init(data: rawCID)
+    }
+    
+    func toJSON() -> JSON {
+        return [
+            ProtoChallenge.serKey : self.data.hexEncodedString()
+        ]
+    }
+    
+    func toUInt32() -> UInt32 {
+        let i32array = self.data.withUnsafeBytes { $0.load(as: UInt32.self) }
+        return i32array
+    }
+}
+
+
+struct ProtoChallenge : ProtoObject {
+
+    static let serKey = "challenge"
+    var data: Data
+    
+    var id: CID {
+        get {
+            return CID(data: self.data[0..<4])!
+        }
+    }
+    
+    init?(data: Data) {
+        if(data.count != 32) {
+            return nil
+        }
+        self.data = data
+    }
+    
+    init?(base64String: String) {
+        guard let data = Data(base64Encoded: base64String) else {
+            return nil
+        }
+        self.init(data: data)
+    }
+    
+    init?(json: JSON) {
+        guard let b64Challenege = json[ProtoChallenge.serKey].string else {
+            return nil
+        }
+        self.init(base64String: b64Challenege)
+    }
+    
+    func toJSON() -> JSON {
+        return [
+            ProtoChallenge.serKey : self.data.base64EncodedString()
+        ]
+    }
+}
---
+++ PassID/proto/ProtoObject.swift
@@ -0,0 +1,18 @@
+//
+//  ProtoObject.swift
+//  PassID
+//
+//  Created by smlu on 16/12/2019.
+//  Copyright © 2019 ZeroPass. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+protocol ProtoObject {
+    static var serKey: String { get }
+    var data: Data { get }
+    
+    init?(data: Data)
+    init?(json: JSON)
+    func toJSON() -> JSON
+}
