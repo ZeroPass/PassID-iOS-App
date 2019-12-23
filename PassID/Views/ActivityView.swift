e29067a17803a384f9bffdc3fe553cef2c784a92
smlu <smlu@s5.net>
2019-12-23 06:37:05 +0100

    Add ActivityIndicator2

--- PassID/Views/ActivityView.swift
+++ PassID/Views/ActivityView.swift
@@ -40,6 +40,32 @@ struct ActivityIndicator: View {
     }
 }
 
+struct ActivityIndicator2: View {
+
+    @State private var isAnimating: Bool = false
+
+    var body: some View {
+        GeometryReader { (geometry: GeometryProxy) in
+            ForEach(0..<5) { index in
+                Circle()
+                    .frame(width: geometry.size.width / 5, height: geometry.size.height / 5)
+                    .scaleEffect(!self.isAnimating ? 1 - CGFloat(index) / 5 : 0.2 + CGFloat(index) / 5)
+                    .offset(y: geometry.size.width / 10 - geometry.size.height / 2)
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+                    .rotationEffect(!self.isAnimating ? .degrees(0) : .degrees(360))
+                    .animation(Animation
+                    .timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5)
+                    .repeatForever(autoreverses: false))
+            }
+        }
+        .frame(width: 60, height: 60)
+        .aspectRatio(1, contentMode: .fit)
+        .onAppear {
+          self.isAnimating = true
+        }
+    }
+}
+
 
 struct ActivityView<Content>: View where Content: View {
 
