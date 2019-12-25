c4125d08e82f35731ce7b4e9c067c20ab31d1406
smlu <smlu@s5.net>
2019-12-25 16:03:34 +0100

    Refactor(PassportView): Add mrtd reader and implement UI to handle mrtd responses

--- PassID/Views/PassportView.swift
+++ PassID/Views/PassportView.swift
@@ -11,13 +11,151 @@ import SwiftUI
 
 struct PassportView: View {
     
+    init(challenge: Binding<ProtoChallenge?>, action: Action, settings: SettingsStore) {
+        self.challenge = challenge
+        self.action    = action
+        self.settings  = settings
+    }
+    
+    var challenge: Binding<ProtoChallenge?>
+    
+    public let action: Action
+    public let settings: SettingsStore
+    
     private var dismissCallback: (() -> Void)? = nil
+    private var passportDataCallback: ((PassportData) -> Void)? = nil
+    
+    @State private var passportNum: String = ""
+    @State private var dob = decadeAgo
+    @State private var doe = tomorrow
+    private let mrtd = MRTDReader()
     
     @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
     
+    private static let tomorrow  = Date().addingTimeInterval(86400)
+    private static let decadeAgo = Calendar.current.date(
+                                        byAdding: .year,
+                                        value: -10,
+                                        to: Date())!
+    
     var body: some View {
-        HStack {
-            Text("Hello, passport screen!")
+        VStack {
+            Spacer()
+                .frame(height: 20)
+            Text("Enter passport details")
+                .foregroundColor(.secondary)
+                .font(.title)
+            
+            Form {
+                HStack {
+                    Text("Passport Number")
+                        Spacer()
+                    TextEdit(placeholder: "<<<<<<<<<", value: $passportNum)
+                        .keyboardType(.asciiCapable)
+                        .textContentType(.name)
+                        .inputValidator({ v in return Utils.isValidPassportNumber(v, forceMinSize: false) })
+                        .foregroundColor(.primary)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 125)
+
+                }.padding([.leading, .trailing])
+                
+                DatePicker("Date of Birth", selection: $dob, in: ...PassportView.decadeAgo, displayedComponents: .date)
+                    .padding([.leading, .trailing])
+                
+                DatePicker("Date Of Expiry", selection: $doe, in: PassportView.tomorrow..., displayedComponents: .date)
+                    .padding([.leading, .trailing])
+                
+                if settings.mrzKey != nil {
+                    Button(action: {
+                        let mrzKey = self.settings.mrzKey!
+                        self.passportNum = mrzKey.mrtdNumber()
+                        self.dob = mrzKey.dateOfBirth()
+                        self.doe = mrzKey.dateOfExpiry()
+                        
+                    }){
+                        HStack {
+                            Spacer()
+                            Text("Fill from storage")
+                                .foregroundColor(.primary)
+                            Spacer()
+                        }
+                    }
+                }
+            }
+            
+            Button(action: {
+                let mrzKey = MRZKey(mrtdNumber: self.passportNum, dateOfBirth: self.dob, dateOfExpiry: self.doe)
+                self.settings.mrzKey = mrzKey
+                self.scanPassport(mrzKey: mrzKey)
+            }){
+                ButtonView(text: "Scan Passport & \(self.action.asString().capitalized)")
+                .foregroundColor(self.validMRZData() ? .primary : Color.secondary.opacity(0.25))
+            }.disabled(!validMRZData())
+            
+            Spacer()
+                .frame(minHeight: 50)
+        }
+        //.onTapGesture { UIApplication.shared.sendAction(#selector(UIView.resignFirstResponder), to: nil, from: nil, for: nil) }
+    }
+    
+    func validMRZData() -> Bool {
+        return Utils.isValidPassportNumber(self.passportNum)
+    }
+    
+    func scanPassport(mrzKey: MRZKey) {
+        mrtd.startSession(mrzKey: mrzKey) { error in
+            if error != nil {
+                Log.error("%@", error!.localizedDescription)
+                PassportView.dispatchOnMainQueue {
+                    Toast.show(message: "Failed to establish passport session")
+                }
+            }
+            else {
+                self.mrtd.readLDSFiles(tags: [.efCOM, .efSOD, .efDG1, .efDG14, .efDG15]) {(ldsFiles, error) in
+                    if error != nil {
+                        Log.error("%@", error!.localizedDescription)
+                        let alert = AlertController(title: "Error", message: "Error reading files from passport.\nPlease try again!", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                        PassportView.dispatchOnMainQueue {
+                            self.showAlert(alert)
+                        }
+                    }
+                    else {
+                        if ldsFiles.count != 5 {
+                            Log.warning("Error: Not all files were read from passport!")
+                            self.mrtd.endSession(withError: "Not all files were read from passport.\nPlease try again!") // TODO: Add error message
+                        }
+                        else {
+                            self.mrtd.internalAuthenticate(challenge: self.challenge.wrappedValue!.data) { csigs, error in
+                                if error != nil {
+                                    Log.error("%@", error!.localizedDescription)
+                                    let alert = AlertController(title: "Error", message: "Failed to sign challenge!\nError: \(error!.localizedDescription)", preferredStyle: .alert)
+                                    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                                    PassportView.dispatchOnMainQueue {
+                                        self.showAlert(alert)
+                                    }
+                                    return
+                                }
+                                
+                                self.mrtd.endSession()
+                                if self.passportDataCallback != nil {
+                                    PassportView.dispatchOnMainQueue {
+                                        let data = PassportData(ldsFiles: ldsFiles, csigs: csigs!)
+                                        self.passportDataCallback!(data)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    static func dispatchOnMainQueue(_ action: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { // delay 3 secs
+            action()
         }
     }
 }
@@ -36,11 +174,18 @@ extension PassportView {
         copy.dismissCallback = callback
         return copy
     }
+    
+    func onPassportData(_ callback: ((PassportData) -> Void)?) -> PassportView {
+        var copy = self
+        copy.passportDataCallback = callback
+        return copy
+    }
 }
 
 struct PassportReaderView_Previews: PreviewProvider {
+    @State static var challenge: ProtoChallenge? = nil
     static var previews: some View {
-        PassportView()
+        PassportView(challenge: $challenge, action: .register, settings: SettingsStore())
             .environment(\.colorScheme, .dark)
     }
 }
