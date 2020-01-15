f4daa0ac12ff6a15265e6ab5b5bcb123e04c3a76
smlu <smlu@s5.net>
2020-01-15 22:55:31 +0100

    Refactor (AlertController): Add title and message style modification methods

--- PassID/Controllers/AlertController.swift
+++ PassID/Controllers/AlertController.swift
@@ -35,4 +35,34 @@ class AlertController: UIAlertController {
     func onDidDisappear(_ cb: @escaping (UIAlertController) -> Void) {
         cbsDidDisappear.append(cb)
     }
+    
+    func setTitleStyle(font: UIFont?, color: UIColor? = nil) {
+           guard let title = self.title else { return }
+           let attributeString = NSMutableAttributedString(string: title)
+           if let titleFont = font {
+               attributeString.addAttributes([NSAttributedString.Key.font : titleFont],
+                                             range: NSMakeRange(0, title.utf8.count))
+           }
+           
+           if let titleColor = color {
+               attributeString.addAttributes([NSAttributedString.Key.foregroundColor : titleColor],
+                                             range: NSMakeRange(0, title.utf8.count))
+           }
+           self.setValue(attributeString, forKey: "attributedTitle")
+       }
+       
+       func setMessageStyle(font: UIFont?, color: UIColor? = nil) {
+           guard let message = self.message else { return }
+           let attributeString = NSMutableAttributedString(string: message)
+           if let messageFont = font {
+               attributeString.addAttributes([NSAttributedString.Key.font : messageFont],
+                                             range: NSMakeRange(0, message.utf8.count))
+           }
+           
+           if let messageColorColor = color {
+               attributeString.addAttributes([NSAttributedString.Key.foregroundColor : messageColorColor],
+                                             range: NSMakeRange(0, message.utf8.count))
+           }
+           self.setValue(attributeString, forKey: "attributedMessage")
+       }
 }
